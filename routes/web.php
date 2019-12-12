30e411821e4a045e232a28870da1e5320a1fa683
Firose Hussain <github@firumon.com>
2019-12-12 07:50:34 +0530

    updates

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([
+    'prefix' => 'mo',
+    'namespace' => 'MO'
+],function(){
+    Route::view('summary','members_offer.summary');
+    Route::view('detail','members_offer.detail');
+    Route::view('print','members_offer.print');
+    Route::post('print','MemberOfferController@print_preview');
+    Route::post('detail',function(){
+        if(request('address')) App\MemberAddress::updateOrCreate(['member' => request('member')],['address' => request('address')]);
+        if(request('number')) App\MemberPhone::updateOrCreate(['member' => request('member')],['number' => request('number')]);
+        if(request('offer')) App\FundCollectionMemberOffer::updateOrCreate(['member' => request('member')],['offer' => request('offer')]);
+        return back();
+    });
+    Route::view('bvpl','babri_verdict.president_letter.index');
+});
+Route::group([
+    'prefix' => 'bv',
+    'namespace' => 'BV'
+],function(){
+    Route::view('pl','babri_verdict.president_letter.index');
+    Route::post('pl','BabriVerdictPresidentLetterController@create');
+});
